e9b583f6d45f6bc8d46aaaf79b2de84d19b6d80f
Aaron Lerch <aaronlerch@gmail.com>
2014-03-06 15:04:16 -0500

    Use full static versioned dmg path

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,9 +5,16 @@
 #  include box_sync
 class box_sync {
 
+  # The correct static resource that redirect to the current is:
+  # https://app.box.com/sync4mac
+  # TODO: Correctly do an HTTP HEAD request there to get the real location
+  # Puppet supports following redirects, but the pkgdmg provider requires the URL
+  # to end in .dmg or .pkg.
+  # :sad panda that understands but is still sad:
+
   package { "BoxSync.pkg":
     provider => 'pkgdmg',
-    source   => "https://app.box.com/sync4mac",
+    source   => "https://app.box.com/shared/static/2d1iwrw24sezez37tby6.dmg",
   }
 
 }
